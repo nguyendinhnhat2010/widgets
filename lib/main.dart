fa2bfa8227906b49f68251428a0b6223dc39bf16
unknown <nmluan.49@gmail.com>
2021-09-28 08:58:10 +0700

    random Number

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,26 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
-  luckyNumber(){
+class _MyAppState extends State<MyApp> {
+  luckyNumber() {
     var randomnumber = "";
     var number1 = Random();
     var number2 = Random();
-    for (int i = 0; i < 6; i++) {
-      randomnumber = randomnumber + "  " + number1.nextInt(max(0, 6)).toString() + number2.nextInt(max(0, 6)).toString();
-    }
+    randomnumber = number1.nextInt(max(0, 6)).toString() +
+        number2.nextInt(max(0, 6)).toString();
     return randomnumber;
   }
-  String randomNum = "Random 6 number";
-  ranDomNumber(){
+
+  String randomNum = "";
+  ranDomNumber() {
     setState(() {
-      randomNum = "Random 6 number : ${luckyNumber()}";
+      randomNum = "${luckyNumber()}";
     });
   }
 
@@ -34,27 +34,49 @@ class _MyAppState extends State<MyApp>{
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.orange,
           title: Center(
-            child: Text('Random Number',
-            style: TextStyle(fontSize: 18, color: Colors.grey,),
+            child: Text(
+              'Random Number',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
         body: Center(
-          child: Column(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(randomNum),
-              FlatButton(
+              for (int i = 0; i < 5; i++)
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: myBoxDecoration(),
+                  child: Text(
+                    "${luckyNumber()}",
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ),
+              ElevatedButton(
                 child: Text('Random'),
                 onPressed: ranDomNumber,
-                color: Colors.blue,
-                textColor: Colors.white,
-                )
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.orange),
+                ),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  BoxDecoration myBoxDecoration() {
+    return BoxDecoration(
+      border: Border.all(color: Colors.orange),
+      borderRadius: BorderRadius.circular(50),
     );
   }
 }
